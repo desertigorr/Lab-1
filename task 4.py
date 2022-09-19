dba8ba5dddd62cbd85ac792da6f21a7e6a2dc1c7
IIgor17 <79518940015a@gmail.com>
2022-09-19 13:11:10 +0300

    Update task 4.py

--- task 4.py
+++ task 4.py
@@ -5,23 +5,40 @@
 import csv
 import random
 
+row_count = 0
+
+
+file = open('result.txt', 'w')
+
+# Вычисляем кол-во строк в таблице и сохраняем значение в переменную
+with open('books.csv', 'r', encoding='cp1251') as f:
+
+    table = csv.reader(f, delimiter=';')
+    
+    for i in table:
+        row_count += 1
 
 with open('books.csv', 'r', encoding='cp1251') as f:
 
     table = csv.reader(f, delimiter=';')
 
-    # Создаем список рандомных ID в виде массива
+
+    # Создаем массив для рандомных ID
     id_list = []
+
     # Сюда будем складировать записи исходя из их ID
     book_list = []
-    # Создаем массив с ID
-    for i in range(20):
-        id_list.append(random.randint(0, 9410))
 
-    # Сортируем, чтобы наполнить book_list за один проход по таблице
+    # Наполняем массив с ID
+    for i in range(20):
+        id_list.append(random.randint(0, row_count))
+
+    # Сортируем по возрастанию, чтобы наполнить book_list за один проход по таблице
     id_list = sorted(id_list)
 
+    # Переменная для ID 
     id = 0
+    # Переменная-счетчик записей для вывода (кол-во таких записей не должно превышать 20)
     count = 0
 
     for row in table:
@@ -36,6 +53,10 @@ with open('books.csv', 'r', encoding='cp1251') as f:
     # Выводим 20 рандомных записей
     for i in range(len(book_list)):
         print(str(book_list[i][3]) + '. '+str(book_list[i][1]) + ' - ' + str(book_list[i][6][6:10]))
+        file.write(str(book_list[i][3]) + '. '+str(book_list[i][1]) + ' - ' + str(book_list[i][6][6:10]))
+        file.write('\n')
+
+file.close()
 
 
 
